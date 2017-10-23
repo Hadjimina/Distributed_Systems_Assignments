7c2c48c34b684a588a7602c1c3028738e8c020c7
Jason <jz.friedman@hotmail.com>
2017-10-23 14:42:21 +0200

    Task 1 completed

--- Assignment_2/VS_davidn_PhoneServer/app/src/main/java/a4/vs/inf/ethz/ch/vs_davidn_phoneserver/task1and2/RestClientActivity.java
+++ Assignment_2/VS_davidn_PhoneServer/app/src/main/java/a4/vs/inf/ethz/ch/vs_davidn_phoneserver/task1and2/RestClientActivity.java
@@ -2,6 +2,7 @@ package a4.vs.inf.ethz.ch.vs_davidn_phoneserver.task1and2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import a4.vs.inf.ethz.ch.vs_davidn_phoneserver.R;
@@ -9,25 +10,36 @@ import a4.vs.inf.ethz.ch.vs_davidn_phoneserver.R;
 public class RestClientActivity extends AppCompatActivity implements SensorListener {
     double temperature = 0.0;
     RawHttpSensor httpSensor;
+    TextSensor textSensor;
+    JsonSensor jsonSensor;
     TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("# RestClientActivity is being created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_client);
 
         //initialize RawHttpSensor
-        httpSensor = new RawHttpSensor();
-        httpSensor.registerListener(this);
-        httpSensor.getTemperature();
+        //httpSensor = new RawHttpSensor();
+        //httpSensor.registerListener(this);
+        //httpSensor.getTemperature();
+
+        //textSensor = new TextSensor();
+        //textSensor.registerListener(this);
+        //textSensor.getTemperature();
+
+        jsonSensor = new JsonSensor();
+        jsonSensor.registerListener(this);
+        jsonSensor.getTemperature();
+
         //initialize TextView
-        txt = (TextView) findViewById(R.id.temp);
+        txt = (TextView) findViewById(R.id.httpRawSensorTxt);
         txt.setText("Temperature: " + temperature);
     }
 
     @Override
     public void onReceiveSensorValue(double value) {
+        Log.d("#", "temperature ist updated.");
         temperature = value;
         txt.setText("Temperature: " + value);
     }
--- Assignment_2/VS_davidn_PhoneServer/app/src/main/java/a4/vs/inf/ethz/ch/vs_davidn_phoneserver/task1and2/TextSensor.java
+++ Assignment_2/VS_davidn_PhoneServer/app/src/main/java/a4/vs/inf/ethz/ch/vs_davidn_phoneserver/task1and2/TextSensor.java
@@ -1,32 +1,45 @@
 package a4.vs.inf.ethz.ch.vs_davidn_phoneserver.task1and2;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by jzfri on 18.10.2017.
  */
 
 class TextSensor extends AbstractSensor {
+    URL url;
+    HttpURLConnection urlConnection;
+    String response = "";
+
     @Override
     public String executeRequest() throws Exception {
-        return null;
+        Log.d("#", "executing request of textSensor.");
+        url = new URL("http://vslab.inf.ethz.ch:8081/sunspots/Spot1/sensors/temperature");
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestProperty("Accept", "text/plain");
+        urlConnection.setRequestProperty("Connection", "close");
+        urlConnection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        String userInput;
+        while((userInput = in.readLine()) != null) {
+            response += userInput + "\r\n";
+        }
+        if(urlConnection != null){
+            urlConnection.disconnect();
+        }
+        Log.d("#", "response is: " + response);
+        return response;
     }
 
     @Override
     public double parseResponse(String response) {
-        return 0;
-    }
-
-    @Override
-    public void getTemperature() {
-
-    }
-
-    @Override
-    public void registerListener(a4.vs.inf.ethz.ch.vs_davidn_phoneserver.task1and2.SensorListener listener) {
-
-    }
-
-    @Override
-    public void unregisterListener(a4.vs.inf.ethz.ch.vs_davidn_phoneserver.task1and2.SensorListener listener) {
-
+       return Double.parseDouble(response);
     }
 }

821f2358926c408f311122e4c2525309658e66a1
David <davidn@student.ethz.ch>
2017-10-26 10:56:56 +0200

    Task2

--- Assignment_3/vs-davidn-chat/app/src/main/java/ch/ethz/inf/vs/a3/Task2/LamportClock.java
+++ Assignment_3/vs-davidn-chat/app/src/main/java/ch/ethz/inf/vs/a3/Task2/LamportClock.java
@@ -2,11 +2,57 @@ package ch.ethz.inf.vs.a3.Task2;
 
 import ch.ethz.inf.vs.a3.clock.Clock;
 
+
 /**
  * Created by david on 26.10.17.
  */
 
-public class LamportClock implements Clock {
+public class LamportClock implements Clock{
 
-    
+    private int time;
+
+    @Override
+    public void update(Clock other) {
+        LamportClock otherLamport = (LamportClock) other;
+        this.time = Math.max(otherLamport.getTime(), this.time);
+    }
+
+    @Override
+    public void setClock(Clock other) {
+        LamportClock otherLamport = (LamportClock) other;
+        this.time = otherLamport.getTime();
+    }
+
+    @Override
+    public void tick(Integer pid) {
+        this.time++;
+    }
+
+    @Override
+    public boolean happenedBefore(Clock other) {
+        LamportClock otherLamport = (LamportClock) other;
+        if(this.time < otherLamport.getTime()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(this.time);
+    }
+
+    @Override
+    public void setClockFromString(String clock) {
+        this.time = Integer.parseInt(clock);
+    }
+
+
+
+    public int getTime() {
+        return this.time;
+    }
+
+
+
 }
---
+++ Assignment_3/vs-davidn-chat/app/src/main/java/ch/ethz/inf/vs/a3/Task2/VectorClock.java
@@ -0,0 +1,10 @@
+package ch.ethz.inf.vs.a3.Task2;
+
+import ch.ethz.inf.vs.a3.clock.Clock;
+
+/**
+ * Created by david on 26.10.17.
+ */
+
+ abstract class VectorClock implements Clock {
+}
